8a922fede29b815a1f4d816698df5031b87fbac2
Jens Eriksson <jenseri@gmail.com>
2020-04-09 23:39:51 +0200

    Update run.py

--- run.py
+++ run.py
@@ -2,14 +2,32 @@ import smbus
 
 address = 0x48
 bus = smbus.SMBus(1)
-
+out = []
 def readA0():
     bus.write_byte(address, 0x40)
-    bus.read_byte(address, 0x40)
+    bus.read_byte(address)
     
-    return bus.read_byte(address, 0x40)
+    return bus.read_byte(address)
+
+def readA1():
+    bus.write_byte(address, 0x41)
+    bus.read_byte(address)
+    
+    return bus.read_byte(address)
+
+def readA2():
+    bus.write_byte(address, 0x42)
+    bus.read_byte(address)
+    
+    return bus.read_byte(address)
+
+def readA3():
+    bus.write_byte(address, 0x43)
+    bus.read_byte(address)
+    
+    return bus.read_byte(address)
 
 if __name__ == "__main__":
     while True:
-        print(readA0())
+        print(readA0(), readA1(), readA2(), readA3())
     
