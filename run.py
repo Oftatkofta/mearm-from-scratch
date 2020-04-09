ff5b2f45d5dbe887676e11fa0876e574de92fef8
Jens Eriksson <jenseri@gmail.com>
2020-04-10 00:36:56 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,28 @@
 import smbus
+import RPi.GPIO as GPIO
+import time
 
 address = 0x48
 bus = smbus.SMBus(1)
 out = []
+
+GPIO.setmode(GPIO.BCM)
+
+basePin = 4
+leftPin = 15
+rightPin = 17
+gripPin = 10
+
+pins = [basePin, leftPin, rightPin, gripPin]
+
+for pin in pins:
+    GPIO.setup(pin,GPIO.OUT)
+    
+base = GPI.PWM(basePin, 50)
+left = GPI.PWM(leftPin, 50)
+right = GPI.PWM(rightPin, 50)
+grip = GPI.PWM(griPin, 50)
+
 def readA0():
     bus.write_byte(address, 0x40)
     bus.read_byte(address)
@@ -26,6 +46,10 @@ def readA3():
     bus.read_byte(address)
     
     return bus.read_byte(address)
+
+except KeyboardInterrupt:
+  p.stop()
+  GPIO.cleanup()
 
 if __name__ == "__main__":
     while True:
